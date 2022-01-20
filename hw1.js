088db00eea9f7cbd171abd0c6fcfacf60c9b27ce
kettesakchhorn <90767281+kettesakchhorn@users.noreply.github.com>
2022-01-20 15:43:09 +0700

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,8 @@
+function myTest(argument) {
+    if (argument.length > 5)
+        return console.log(true);
+    else
+        return console.log(false);
+}
+myTest("kirirom");
+myTest("KIT");
---
+++ hw1.ts
@@ -0,0 +1,9 @@
+function myTest(argument: string){
+    if(argument.length >5 )
+    return console.log(true)
+    else
+    return console.log(false)
+}
+
+myTest("kirirom")
+myTest("KIT")
